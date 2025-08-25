9df73267435b7894695b7328377370939b7e96ae
admin735 <hesh@andiamoventures.co>
2025-08-25 17:26:48 -0400

    Deleted content.config.ts

--- src/config/index.ts
+++
@@ -1,8 +0,0 @@
-// Central configuration exports
-export { content } from './content.config';
-export { quizConfig } from './quiz.config';
-export { theme } from './theme.config';
-export { apiConfig } from './api.config';
-
-// Type exports
-export type { QuizQuestion } from './quiz.config';
