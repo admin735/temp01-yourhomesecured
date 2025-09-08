68ea002dbafcca0166d4d68438a7b4d0f804ca1a
admin735 <hesh@andiamoventures.co>
2025-09-08 10:10:43 -0400

    Remove TrustedForm from ComplianceScripts component

--- src/core/components/ComplianceScripts.tsx
+++ src/core/components/ComplianceScripts.tsx
@@ -1,24 +1,11 @@
 import { useEffect } from 'react';
-import { loadJornayaScript, loadTrustedFormScript } from '../utils/compliance';
 import { complianceConfig } from '../../config/compliance.config';
 
 export const ComplianceScripts: React.FC = () => {
   useEffect(() => {
-    const loadScripts = async () => {
-      console.log('Initializing compliance scripts...');
-      
-      // Only load Jornaya here if you want it on page load
-      // Otherwise, remove this too and load both at contact form step
-      if (complianceConfig.jornaya.enabled) {
-        console.log('Loading Jornaya script...');
-        await loadJornayaScript();
-      }
-      
-      // TrustedForm will be loaded at contact form step instead
-      console.log('Compliance scripts initialization complete');
-    };
-
-    loadScripts();
+    // Compliance scripts now load at contact form step instead of page load
+    // This provides better compliance focus and performance
+    console.log('Page-level compliance: Scripts will load at contact form step');
   }, []);
 
   // This component doesn't render anything visible
